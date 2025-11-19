15f1a2ff7b124460a40abf523ab94b96255ed846
datndse171888 <datndse171888@fpt.edu.vn>
2025-11-19 17:38:26 +0700

    save orderId to localStorage

--- src/pages/public/Product.tsx
+++ src/pages/public/Product.tsx
@@ -192,6 +192,8 @@ export const Product: React.FC = () => {
       } else {
         const data: OrderResponse = response.data;
         console.log('Order submitted successfully:', data);
+
+        localStorage.setItem('orderId', data.orderId);
         
         // mở payment URL ở 1 tab khác nếu `paymentUrl` tồn tại
         window.open(data.paymentUrl, '_blank');
